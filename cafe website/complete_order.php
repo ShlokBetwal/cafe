65c144212647c6994588f442dec316493d453dcc
ShlokBetwal <betwalshlok@gmail.com>
2026-07-15 23:32:30 +0530

    Change database connection to cloud service
    Updated database connection details to use cloud service.

--- cafe website/complete_order.php
+++ cafe website/complete_order.php
@@ -1,4 +1,12 @@
 <?php
+$servername = "bvhujbzywochddf20hir-mysql.services.clever-cloud.com";
+$username = "usbzg45wtej4xap";
+$password = "XfZ3BBYK48HJGHcqJITO"; 
+$dbname = "bvhujbzywochddf20hir";
+$port = 3306;
+
+// Establishes connection using the cloud port layout
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 $conn = new mysqli("localhost", "root", "", "cafe_db");
 if (!empty($_POST['id'])) {
     $id = (int) $_POST['id'];
